ef0dc2a093811d2b853e1af1eb91fe8fb5f82587
Alan Maldonado <alan.maldonado@gmail.com>
2024-03-24 03:04:25 -0600

    🎉 Layouts in Tailwind with Flexbox

---
+++ src/components/Flexbox.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="flex flex-wrap items-end justify-center gap-2">
+    <div
+      v-for="i in 15"
+      :key="i"
+      class="h-14 w-14 flex-shrink-0 bg-gray-500 p-4 text-center text-white"
+      :style="`height: ${i * 30 + 30}px`"
+    >
+      {{ i }}
+    </div>
+  </section>
+</template>
